189c564447f459a909e5d235abbb3c40f25c9f63
volodin <volodin@arnica.pro>
2024-07-16 11:48:33 +0500

    настройки LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,3 +3,12 @@ require('lspconfig').intelephense.setup {
   filetypes = { 'php' },
   root_dir = require('lspconfig/util').root_pattern('.git', 'composer.json')
 }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+  }
+)
